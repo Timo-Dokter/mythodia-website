23a14d9bb74c098be18d82f67b680de89616cb6d
Timo Dokter <timo.dokter@gmail.com>
2023-01-08 19:26:22 +0100

    remove django project

--- README.md
+++
@@ -1,2 +0,0 @@
-# mythodia-website
-A website for my dungeons and dragons party and there adventures in the world of Mythodia
--- mythodia/modules/core/views/dev.py
+++
@@ -1,9 +0,0 @@
-from django.views import View
-from django.shortcuts import render
-
-
-class Dev(View):
-    template_name = "core/dev.html"
-
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
