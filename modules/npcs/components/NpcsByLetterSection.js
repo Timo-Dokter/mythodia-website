6d4f86fd526af7e538bb54607aed3db849c1ff92
Timo Dokter <timo.dokter@gmail.com>
2023-01-29 15:08:15 +0100

    fix break bug

--- modules/npcs/components/NpcsByLetterSection.js
+++ modules/npcs/components/NpcsByLetterSection.js
@@ -4,7 +4,10 @@ export const NpcsByLetterSection = ({ npcsByLetter }) => {
   return (
     <div className="columns-3">
       {Object.entries(npcsByLetter).map(([firstLetter, npcs]) => (
-        <section className="flex flex-col mb-2" key={firstLetter}>
+        <section
+          className="flex flex-col mb-2 break-inside-avoid"
+          key={firstLetter}
+        >
           <h4 className="text-xl">{firstLetter}</h4>
           <ul className="list-inside list-disc">
             <NpcLinks npcs={npcs} />
